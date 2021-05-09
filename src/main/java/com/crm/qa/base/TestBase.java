cbc0d1324bbfc58e1a4ff55e4741dc05f48b9d6a
Debanjan Bhowmick <bhowmick.debanjan@gmail.com>
2021-05-09 11:11:11 +0530

    modifed the Chromedriver path

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -41,7 +41,7 @@ public class TestBase {
 	String browserName = 	prop.getProperty("browser");
 	if(browserName.equals("chrome")) {
 		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", "/Users/Debanjan/Downloads/chromedriver.exe");	
+			System.setProperty("webdriver.chrome.driver", "C:/Users/Debanjan/Desktop/QA & others/Driver chrome/chromedriver.exe");	
 			driver = new ChromeDriver(); 
 		}
 		else if(browserName.equals("FF")){
